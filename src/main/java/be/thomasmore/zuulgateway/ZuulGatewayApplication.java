1dcb1f619265bb6d44e3463d5dce746fe56119c3
JelleVLD <38531477+JelleVLD@users.noreply.github.com>
2019-11-23 14:57:37 +0100

    login werkt met hardcoded users

---
+++ src/main/java/be/thomasmore/zuulgateway/ZuulGatewayApplication.java
@@ -0,0 +1,26 @@
+package be.thomasmore.zuulgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class ZuulGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulGatewayApplication.class, args);
+	}
+}
